159cf57594c8dc3de81c6dcd1adae5e33392120a
Andres Ossa <andres.ossa@cision.com>
2017-11-29 23:44:11 -0500

    A bit of optimization

--- js/app.js
+++ js/app.js
@@ -10,19 +10,42 @@ $(function() {
     //Hash of views
     const views = {
         [HOME]: { el: $home, fn: function(){}, started: false },
-        [DESIGN]: { el: $design, fn: initDesignEvents, started: false },
-        [ADVERTISING]: { el: $advertising, fn: initAdvertisingEvents, started: false }
+        [DESIGN]: { el: $design, fn: startCards, started: false },
+        [ADVERTISING]: { el: $advertising, fn: startCards, started: false }
     };
 
     //Add Navigation Listeners
     $('.advertising', $home).click(navigationHandler(ADVERTISING));
     $('.design', $home).click(navigationHandler(DESIGN));
     $(window).on('popstate', popStateHandler);
+    //Create the Home state
+    window.history.replaceState({active: HOME}, HOME);
 
     /**
-     * Takes in a viewHandle and returns a function expecting an event object
+     * Handles the navigation events from the browser buttons:
+     * Obtains the active view defined in the state object and activates it.
+     * @param {PopStateEvent} event popStateEvent fired by the browser's navigation buttons.
+     */
+    function popStateHandler(event) {
+        var stateData = event && event.originalEvent && event.originalEvent.state;
+        if (stateData && stateData.active) {
+            activateView(stateData.active);
+        }
+    }
+
+    /**
+     * Creates a browser history item represented by given stateData and named name.
+     * @param {Object} stateData data that represents the state being saved into history 
+     * @param {String} name name to give to the history item 
+     */
+    function createHistoryEntry(stateData, name) {
+        window.history.pushState(stateData, name);
+    }
+
+    /**
+     * Takes in a viewHandle and returns a function to be used as eventHandler
      * @param {String} viewHandle id of the view to go to.
-     * @returns {Function} a handler function that expects an event object
+     * @returns {Function} an eventHandler function
      */
     function navigationHandler(viewHandle) {
         /**
@@ -37,77 +60,46 @@ $(function() {
 
     /**
      * Shows the view identified by viewHandle
-     * Hides all the other views
      * @param {String} viewHandle id of the view to activate
      */
     function activateView(viewHandle) {
-        viewHandle = viewHandle || HOME;
+        views[viewHandle].el.show();
         Object.keys(views).forEach(function(key) {
+            var view = views[key];
+            
             if (key === viewHandle) {
-                var viewData = views[key];
-                viewData.el.show();
-                if (!viewData.started) {
-                    viewData.fn();
-                    viewData.started = true;
+                view.el.show();
+                if (!view.started) {
+                    view.fn(view.el);
+                    view.started = true;
                 }
             } else {
-                views[key].el.hide();
+                view.el.hide();
             }
         });
     }
 
-    /**
-     * Creates a browser history item represented by given stateData and named name.
-     * @param {Object} stateData data that represents the state being saved into history 
-     * @param {String} name name to give to the history item 
-     */
-    function createHistoryEntry(stateData, name) {
-        window.history.pushState(stateData, name);
-    }
-
-    /**
-     * Handles the navigation events from the browser buttons:
-     * Obtains the active view defined in the state object and activates it.
-     * @param {PopStateEvent} event popStateEvent fired by the browser's navigation buttons.
-     */
-    function popStateHandler(event) {
-        var stateData = event && event.originalEvent && event.originalEvent.state;
-        if (stateData && stateData.active) {
-            activateView(stateData.active);
-        }
-    }
-
-    function initDesignEvents() {
-        $('.card-container').click(function(ev) {
+    function startCards(context) {
+        var cardElements = $('.card-container', context);
+        cardElements.click(function(ev) {
             var element = ev.currentTarget;
             element.classList.toggle('active');
-            killClass('.card-container', 'active', element.id);
+            killClass(cardElements, 'active', element.id);
         });
     }
 
-    function initAdvertisingEvents() {
-
-    }
-
     /**
-     * 
-     * @param {String|DOMSelector} selector a valid DOMSelector string 
+     * Removes the given className from Elements matching selector.
+     * If idToExclude is supplied, the class won't be removed from the Element with that id 
+     * @param {Element} elements a collection of Elements 
      * @param {String} className name of class to remove from elements matched by selector
-     * @param {String} idToExclude id of the HTMLDOMNode to exclude from removal of the class
+     * @param {String} idToExclude id of the Element to exclude from removal of the class
      */
-    function killClass(selector, className, idToExclude) {
-        if (idToExclude) {
-            $(selector).each(function(index, element) {
-                if (element.id !== idToExclude) {
-                    element.classList.remove(className);
-                }
-            });
-        } else {
-            //Yay for implicit iteration
-           $(selector).removeClass(className); 
-        }
+    function killClass(elements, className, idToExclude) {
+        elements.each(function(index, element) {
+            if (element.id !== idToExclude) {
+                element.classList.remove(className);
+            }
+        });
     }
-
-    //Create the Home state
-    window.history.replaceState({active: HOME}, HOME);
 });
