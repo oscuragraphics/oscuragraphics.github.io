1c701cf20b19e9be9ef951e4b6ec6377141ef934
Andres Ossa <andres.ossa@cision.com>
2017-12-04 23:24:14 -0500

    Added Advertising

--- js/app.js
+++ js/app.js
@@ -1,127 +1,55 @@
-// (function(_$){
-
-// }($))
-
 $(function() {
-    debugger;
-    class View {
-        constructor(selector, initFunction, images, name) {
-            this.name = name || selector;
-            this._el = $(`#${selector}`);
-            this._fn = initFunction;
-            this._started = false;
-            this._images = images;
-        }
+    const $cardElements = $('.card-container');
 
-        show() {
-            this._el.show();
+    $cardElements.click(function(ev) {
+        var $element = $(this);
+        
+        killClass($cardElements, 'active', this.id);
+        if (!$element.hasClass('active')) {
+            $element.addClass('active');
+            initSlider($element);
         }
+    });
 
-        hide() {
-            this._el.hide();
-        }
+    function initSlider($element) {
+        var $popUpCard = $('.card-popup', $element);
 
-        run() {
-            this.show();
-            if (!this._started) {
-                this._started = true;
-                this._fn(this._el);
+        var $closingButton = $('.closing-button', $popUpCard);
+        var $slides = $('.slide-image', $popUpCard);
+        var $slider = $('.slider', $popUpCard);
+
+        $closingButton.click(function(ev) {
+            ev.stopPropagation();
+            if ($slider.hasClass('slick-initialized')) {
+                $slider.slick('unslick');
             }
-            window.location.hash = this.name;
-        }
-    }
+            if ($element.hasClass('active')){
+                $element.removeClass('active');
+            }
+        });
 
-    class Views {
-        constructor(views) {
-            this._views = views;
-            views.forEach(view => {
-                this[view.name] = view._el;
-                this[`_${view.name}`] = view;
-            });
-        }
-
-        activateView(viewName) {
-            this._views.forEach(view => {
-                if (view.name === viewName) {
-                    view.run();
-                } else {
-                    view.hide();
-                }
+        if ($slides.length > 1) {
+            $slider.slick({
+                draggable: false, 
+                infinite: true,
+                slidesToShow: 1,
+                variableWidth: true
             });
         }
     }
 
-    
-    //View handles
-    const HOME = 'home';
-    const ADVERTISING = 'advertising';
-    const DESIGN = 'design'; 
-    const DESIGN_IMAGES = ['cats1.jpg', 'cats2.jpg', 'cats3.jpg', 'cats4.jpg', 'cats5.jpg'];
-    const views = new Views([
-        new View(HOME, () => {}),
-        new View(DESIGN, startCards),
-        new View(ADVERTISING, startCards)
-    ]);
-
-    //Add Navigation Listeners
-    // $('.advertising', views[HOME]).click(navigationHandler(ADVERTISING));
-    // $('.design', views[HOME]).click(navigationHandler(DESIGN));
-    // $(window).on('popstate', popStateHandler);
-    // window.history.replaceState({active: HOME}, HOME);
-    
-
     /**
-     * Handles the navigation events from the browser buttons:
-     * Obtains the active view defined in the state object and activates it.
-     * @param {PopStateEvent} event popStateEvent fired by the browser's navigation buttons.
+     * Removes the given className from Elements matching selector.
+     * If idToExclude is supplied, the class won't be removed from the Element with that id 
+     * @param {jQuery} elements a collection of Elements 
+     * @param {String} className name of class to remove from elements matched by selector
+     * @param {String} idToExclude id of the Element to exclude from removal of the class
      */
-    function popStateHandler(event) {
-        var stateData = event && event.originalEvent && event.originalEvent.state;
-        if (stateData && stateData.active) {
-            views.activateView(stateData.active);
-        }
-    }
-
-    /**
-     * Creates a browser history item represented by given stateData and named name.
-     * @param {Object} stateData data that represents the state being saved into history 
-     * @param {String} name name to give to the history item 
-     */
-    function createHistoryEntry(stateData, name) {
-        window.history.pushState(stateData, name);
-    }
-
-    /**
-     * Takes in a viewHandle and returns a function to be used as eventHandler
-     * @param {String} viewHandle id of the view to go to.
-     * @returns {Function} an eventHandler function
-     */
-    function navigationHandler(viewHandle) {
-        /**
-         * @param {Event} event event object associated with the event
-         */
-        return function(event) {
-            event.preventDefault();
-            views.activateView(viewHandle);
-            createHistoryEntry({active: viewHandle}, viewHandle);
-        };        
-    }
-
-
-    
-
-    function initImageSlider() {
-        $('.slider-control.right').click((ev) => {
-            ev.preventDefault();
-            ev.stopPropagation();
-            console.log('Right');
-        });
-        $('.slider-control.left').click((ev) => {
-            ev.preventDefault();
-            ev.stopPropagation();
-            console.log('Left');
+    function killClass(elements, className, idToExclude) {
+        elements.each(function(index, element) {
+            if (element.id !== idToExclude) {
+                element.classList.remove(className);
+            }
         });
     }
-
-    
 });
